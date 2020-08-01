279a2566f81d760b00fb8682047d38fdc9953c23
Xy718 <869839000@qq.com>
2020-08-01 16:56:10 +0800

    feat：创建了网关API模块

---
+++ GATEWAY/src/test/java/xyz/xy718/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xy718.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
